f88ad1899023b39121bf18ff19f39bfb793be37a
Ritvik Thakur <ritvik.thakur04@gmail.com>
2023-11-11 15:32:06 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your animations can be handled in CSS, so you may not need JavaScript at this time.
+    // However, if you want to add more complex interactions later, this is where you would put your JS code.
+});
